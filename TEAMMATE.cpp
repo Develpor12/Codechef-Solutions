93e9a135f1ee0895ab6a79992c722cb8023b63e3
Rakesh <rsenwar448@gmail.com>
2018-10-20 21:24:49 +0530

    Update TEAMMATE.cpp

--- TEAMMATE.cpp
+++ TEAMMATE.cpp
@@ -1,9 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-ll fac[1000001],mod=1000000007;
-ll h[1000001];
-void __init() {
+ll fac[1000001],mod=1000000007, h[1000001];
+void init() {
 	fac[0] = 1;
 	for(int i=1;i<1000001;i++)
 		fac[i] = (fac[i-1]*i)%mod;
@@ -14,12 +13,8 @@ ll fastmod(ll  base,ll p)
     if(p ==0)
         return 1;
     else if(p&1)
-    {
- 
         return (base*fastmod(base,p-1))%mod;
-    }
-    else
-    {
+    else {
         ll k =  fastmod(base,p/2);
         return (k*k)%mod;
     }
@@ -35,7 +30,7 @@ ll solve(ll n) {
 
 int main() {
 	ll t,n,s[100001];
-	__init();
+	init();
 	cin>>t;
 	while(t--) {
 		ll ans = 1,ctr = 1,p=0;
@@ -51,14 +46,13 @@ int main() {
 				h[p++] = ctr;
 				ctr = 1;
 			}
-		//for(int i=0;i<p;i++)cout<<h[i]<<" ";cout<<endl;
 		for(int i=p-1;i>=0;i--) {
 			if((h[i])&1) {
-				ans = ((ans * h[i-1])%mod * (solve(h[i]+1))%mod)%mod;//cout<<"1 "<<ans<<" \n";
+				ans = ((ans * h[i-1])%mod * (solve(h[i]+1))%mod)%mod;
 				h[i-1] -= 1;
 			}
 			else {
-				ans = (ans * solve(h[i]))%mod;//cout<<"2 "<<ans<<" \n";
+				ans = (ans * solve(h[i]))%mod;
 			}
 		}		
 		cout<<ans<<endl;						
